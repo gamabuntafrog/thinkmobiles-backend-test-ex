1e15c08a640701efd9a10c9266deb8e51e5dfcb2
gamabuntafrog <kirik.slaw@gmail.com>
2023-03-25 00:22:47 +0200

    added pagination to getUserEvents

--- controllers/events/getUserEvents.js
+++ controllers/events/getUserEvents.js
@@ -6,24 +6,36 @@ const getUserEvents = async (req, res) => {
     const {currentUserId} = req;
     const {id} = req.params;
 
-    const user = await UserForEvents.findById(id).populate('events')
+    const page = req.query.page || 0;
+    const limit = 5;
+
+    const start = (page * limit);
+    const end = ((page * limit) + 5);
+
+    const user = await UserForEvents.findById(id)
+        .slice('events', [start, end])
+        .populate('events')
+
 
     if (!user?.events || user?.creator?.toString() !== currentUserId.toString()) {
         throw new NotFound('Events for this user does not exist');
     }
-
-    const {events} = user;
-
-    const formattedEvents = events.map((event) => {
+    const formattedEvents = user.events.map((event) => {
         const {title, description, startDate, endDate, _id} = event
 
         return {title, description, startDate, endDate, _id}
     })
 
+    const {events: notPopulatedEvents} = await UserForEvents.findById(id)
+
+    const pages = Math.ceil(notPopulatedEvents.length / limit)
+
+
     res.status(200).json({
         message: 'success',
         code: 200,
         data: {
+            pages: pages,
             events: formattedEvents
         }
     })
