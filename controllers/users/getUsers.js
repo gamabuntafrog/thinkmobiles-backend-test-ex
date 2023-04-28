a19dd7703bb1e4072acba971ad41e296389fce29
Yaroslaw Kyrylenko <kirik.slaw@gmail.com>
2023-04-28 13:09:36 +0300

    created different version of getUsers.js

--- controllers/users/getUsers.js
+++ controllers/users/getUsers.js
@@ -11,6 +11,9 @@ const { Types } = require('mongoose')
 // створити декілька агрегацій
 // зробити пошук по полю creator
 
+// getUsersSecond
+// Робить всі операції по сортуванню фільтрації та створенню полів в sub-pipeline який знаходиться в середині $lookup
+
 const getUsersSecond = async (req, res) => {
   const { currentUserId, query } = req
 
@@ -22,10 +25,114 @@ const getUsersSecond = async (req, res) => {
   if (sortBy) {
     sorting[sortBy] = variant
   }
+
+  const match = {
+    _id: new Types.ObjectId(currentUserId)
+  }
+
+  const addNextEventDateField = {
+    $addFields: {
+      nextEventDate: {
+        $ifNull: [
+          {
+            $first: '$events.startDate'
+          },
+          null
+        ]
+      }
+    }
+  }
+
+  const pipeline = [
+    { $match: match },
+    {
+      $lookup: {
+        from: 'user_for_events',
+        localField: 'usersForEvents',
+        foreignField: '_id',
+        as: 'usersForEvents',
+        pipeline: [
+          {
+            $facet: {
+              countOfNotFilteredDocuments: [{ $count: 'count' }],
+              documents: [
+                {
+                  $lookup: {
+                    from: 'user_events',
+                    localField: 'events',
+                    foreignField: '_id',
+                    as: 'events',
+                    pipeline: [
+                      { $match: { $expr: { $gte: ['$startDate', new Date()] } } },
+                      { $limit: 1 },
+                      { $project: { startDate: 1, endDate: 1 } }
+                    ]
+                  }
+                },
+                ...(sortBy
+                  ? [
+                      ...(sortBy === 'nextEventDate' ? [addNextEventDateField] : []),
+                      {
+                        $sort: sorting
+                      }
+                    ]
+                  : []),
+                {
+                  $skip: skip
+                },
+                {
+                  $limit: limit
+                },
+                ...(sortBy !== 'nextEventDate' ? [addNextEventDateField] : []),
+                {
+                  $project: {
+                    username: 1,
+                    firstName: 1,
+                    lastName: 1,
+                    email: 1,
+                    nextEventDate: 1,
+                    eventsCount: 1,
+                    phoneNumber: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $project: {
+              countOfNotFilteredDocuments: { $first: '$countOfNotFilteredDocuments.count' },
+              documents: 1
+            }
+          }
+        ]
+      }
+    },
+    {
+      $project: {
+        usersForEvents: { $first: '$usersForEvents' }
+      }
+    }
+  ]
+
+  const [
+    {
+      usersForEvents: { documents, countOfNotFilteredDocuments }
+    }
+  ] = await User.aggregate(pipeline).exec()
+  const pages = getPagesCount(countOfNotFilteredDocuments, limit)
+
+  res.status(200).json({
+    message: 'success',
+    code: 200,
+    data: {
+      pages: pages,
+      users: documents
+    }
+  })
 }
 
 const getUsers = async (req, res) => {
-  // return await getUsersSecond(req, res)
+  return await getUsersSecond(req, res)
 
   const { currentUserId, query } = req
 
